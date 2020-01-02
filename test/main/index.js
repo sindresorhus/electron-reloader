491b5a77ac85f8bc77138a9f67549c3bd254b1a7
Elijah <ejsisgreat@gmail.com>
2020-01-02 06:48:40 -0600

    Put the main file in a sub-directory

---
+++ test/main/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+require("../..")(module, {
+	debug: true
+});
+
+let mainWindow;
+
+(async () => {
+	await app.whenReady();
+
+	mainWindow = new BrowserWindow();
+	await mainWindow.loadFile(path.join(__dirname, "../index.html"));
+})();
